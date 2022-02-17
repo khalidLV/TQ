55625b7fbd6e41d7a96ac82db6730b43b1b50a1e
khalid <khalidobalharbi@gmail.com>
2022-02-17 22:50:17 +0300

    update composer

--- public/index.php
+++ public/index.php
@@ -31,9 +31,9 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
 
 
+require('../vendor/autoload.php');
 
 
 /*
